8ed7ca05a9dc385f858ef2737a2480c2228726b9
rizwan230698 <rizwananwar230698@gmail.com>
2020-06-24 22:19:42 +0530

    Updates

---
+++ precache-manifest.9795b9da8e984a5575bd95b9dc977211.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1215b6f1404a4c6bf887bdbef9075e3",
+    "url": "/punk-beer/index.html"
+  },
+  {
+    "revision": "9df3cfa5514e9cd36888",
+    "url": "/punk-beer/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "39a739b75568dbccb2de",
+    "url": "/punk-beer/static/css/main.0bf977bb.chunk.css"
+  },
+  {
+    "revision": "9df3cfa5514e9cd36888",
+    "url": "/punk-beer/static/js/2.6a25aac1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/punk-beer/static/js/2.6a25aac1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39a739b75568dbccb2de",
+    "url": "/punk-beer/static/js/main.549c3985.chunk.js"
+  },
+  {
+    "revision": "45bd1fd60fa726e34b7c",
+    "url": "/punk-beer/static/js/runtime-main.583a8c8a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/punk-beer/precache-manifest.9795b9da8e984a5575bd95b9dc977211.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/punk-beer/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
