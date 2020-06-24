d4a13faa1700cc72766a44e8b7686cfdb28f9bec
rizwan230698 <rizwananwar230698@gmail.com>
2020-06-24 23:21:44 +0530

    Content min-height fixed

--- src/components/Empty/style.js
+++ src/components/Empty/style.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const EmptyContainer = styled.div`
-  height: calc(100vh - 294px);
+  height: calc(100vh - 204px);
   display: flex;
   justify-content: center;
   align-items: center;
