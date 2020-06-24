26d4ab96a1165e45cb8a3466539f213965992364
rizwan230698 <rizwananwar230698@gmail.com>
2020-06-24 09:08:52 +0530

    Added a layout component

---
+++ src/components/layout/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Layout } from 'antd';
+
+import { HeaderX, Logo, OptionLink, ContentX, FooterX } from './style';
+
+const LayoutComponent = ({ children }) => (
+  <Layout>
+    <HeaderX>
+      <Logo>Beans Love Beers</Logo>
+      <OptionLink to="/">Home</OptionLink>
+      <OptionLink to="/favourite">Favourite</OptionLink>
+    </HeaderX>
+    <ContentX>{children}</ContentX>
+    <FooterX style={{ textAlign: 'center' }}>
+      Punk Beer ©2020 Created by Rizwan Anwar
+    </FooterX>
+  </Layout>
+);
+
+export default LayoutComponent;
---
+++ src/components/layout/style.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import { Layout } from 'antd';
+import { Link } from 'react-router-dom';
+
+const { Header, Content, Footer } = Layout;
+
+export const HeaderX = styled(Header)`
+  background-color: #00d1b2;
+  padding: 10px 50px;
+  display: flex;
+  align-items: center;
+`;
+
+export const Logo = styled.h1`
+  margin-right: auto;
+  margin-bottom: 0;
+  color: #fff;
+  font-size: 28px;
+  font-weight: 300;
+`;
+
+export const OptionLink = styled(Link)`
+  padding: 0 10px;
+`;
+
+export const ContentX = styled(Content)`
+  background-color: #fff;
+  padding: 0 50px;
+  min-height: calc(100vh - 139px);
+  padding: 24px 50px;
+`;
+
+export const FooterX = styled(Footer)`
+  background-color: #fff;
+`;
