1e21bcc4ec8e1973c591dac1f164880697fd366a
rizwan230698 <rizwananwar230698@gmail.com>
2020-06-24 19:03:36 +0530

    Added favorite beer route

--- src/pages/favourite/index.js
+++ src/pages/favourite/index.js
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Row, Col } from 'antd';
 
-const Favourite = () => <h1>Favourite</h1>;
+import { BeerContext } from '../../context/beerProvider';
+import BeerCard from '../../components/beerCard';
+import Empty from '../../components/Empty';
+
+const Favourite = () => {
+  const {
+    state: { favouriteBeers },
+  } = useContext(BeerContext);
+
+  if (favouriteBeers.length === 0) {
+    return <Empty />;
+  }
+
+  return (
+    <Row gutter={[16, 16]}>
+      {favouriteBeers.map((beer) => (
+        <Col lg={8} md={8} sm={12} xs={12} key={beer.id}>
+          <BeerCard beer={beer} />
+        </Col>
+      ))}
+    </Row>
+  );
+};
 
 export default Favourite;
