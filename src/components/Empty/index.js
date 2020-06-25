d1d0317551cd23b2344492f0c642a8722da8d8d1
rizwan230698 <rizwananwar230698@gmail.com>
2020-06-25 09:37:51 +0530

    Added beer image

--- src/components/Empty/index.js
+++ src/components/Empty/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Empty as AntdEmpty } from 'antd';
 
+import BeerImage from '../../asests/beer.png';
 import { EmptyContainer } from './style';
 
 const Empty = () => (
   <EmptyContainer>
-    <AntdEmpty description="No favourite beer found." />
+    <AntdEmpty description="No favourite beer found." image={BeerImage} />
   </EmptyContainer>
 );
 
