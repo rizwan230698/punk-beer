a78f48f81798f7dfdee9a6cddd0a368a50a20674
rizwan230698 <rizwananwar230698@gmail.com>
2020-06-25 10:40:50 +0530

    showing tagline on card hover

--- src/components/beerCard/index.js
+++ src/components/beerCard/index.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { Tooltip } from 'antd';
 import { StarOutlined, StarFilled } from '@ant-design/icons';
 
 import {
@@ -13,7 +14,7 @@ import { shortDescription } from '../../utils';
 import { BeerContext } from '../../context/beerProvider';
 
 const BeerCard = React.forwardRef(({ beer }, ref) => {
-  const { id, name, description, image_url } = beer;
+  const { id, name, description, image_url, tagline } = beer;
   const {
     state: { favourites },
     addToFavourite,
@@ -21,20 +22,22 @@ const BeerCard = React.forwardRef(({ beer }, ref) => {
   } = useContext(BeerContext);
 
   return (
-    <CardX hoverable>
-      <ImageContainer ref={ref}>
-        <Image src={image_url} alt={name} />
-      </ImageContainer>
-      <Content>
-        <Title>{name}</Title>
-        <Description>{shortDescription(description)}</Description>
-      </Content>
-      {!favourites[id] ? (
-        <StarOutlined onClick={() => addToFavourite(beer)} />
-      ) : (
-        <StarFilled onClick={() => removeFromFavourite(beer)} />
-      )}
-    </CardX>
+    <Tooltip title={tagline} placement="bottom" color="#0D0D0D">
+      <CardX hoverable>
+        <ImageContainer ref={ref}>
+          <Image src={image_url} alt={name} />
+        </ImageContainer>
+        <Content>
+          <Title>{name}</Title>
+          <Description>{shortDescription(description)}</Description>
+        </Content>
+        {!favourites[id] ? (
+          <StarOutlined onClick={() => addToFavourite(beer)} />
+        ) : (
+          <StarFilled onClick={() => removeFromFavourite(beer)} />
+        )}
+      </CardX>
+    </Tooltip>
   );
 });
 
